6af21dbc8c6b89bdafbc37659fa51e630dc837b2
fnm-dev <f_nmachado@hotmail.com>
2022-08-18 10:01:45 -0300

    Downloading audio files

---
+++ main.py
@@ -0,0 +1,23 @@
+import pytube
+import moviepy.editor as mp
+import re
+import os
+
+link = input("Enter the link of the video you want to download: ")
+path = r"D:\Users\FelipePC\Downloads"
+yt = pytube.YouTube(link)
+
+print("Downloading...")
+yt.streams.get_highest_resolution().download(path)
+print("Download completed.")
+
+print("Converting...")
+for file in os.listdir(path):
+    if re.search('mp4', file):
+        mp4_path = os.path.join(path, file)
+        mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
+        new_file = mp.AudioFileClip(mp4_path)
+        new_file.write_audiofile(mp3_path)
+        os.remove(mp4_path)
+
+print("Successful!")
