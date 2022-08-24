9a1015568ab3d4cd9c13ac694bc09579117bfe29
Felipe Machado <f_nmachado@hotmail.com>
2022-08-24 00:06:03 -0300

    Update: while true and clearing terminal

--- main.py
+++ main.py
@@ -2,30 +2,36 @@ import pytube
 import moviepy.editor as mp
 import re
 import os
+from time import sleep
 
-link = input("Enter the link of the video you want to download: ")
-option = input("Audio or video? (a/v) ")
-path = r"D:\Users\FelipePC\Downloads"
-yt = pytube.YouTube(link)
 
-print("Title:", yt.title)
-print("Author:", yt.author)
-print("Published date:", yt.publish_date.strftime("%Y-%m-%d"))
-print("Number of views:", yt.views)
-print("Length of video:", yt.length, "seconds")
+while True:
+    link = input("Enter the link of the video you want to download: ")
+    option = input("Audio or video (a/v)? ")
+    path = r"D:\Users\FelipePC\Downloads"
+    yt = pytube.YouTube(link)
 
-print("Downloading...")
-yt.streams.get_highest_resolution().download(path)
-print("Download completed.")
+    print("\nTitle:", yt.title)
+    print("Author:", yt.author)
+    print("Published date:", yt.publish_date.strftime("%Y-%m-%d"))
+    print("Number of views:", yt.views)
+    print("Length of video:", yt.length, "seconds")
 
-if option == 'a':
-    print("Converting...")
-    for file in os.listdir(path):
-        if re.search('mp4', file):
-            mp4_path = os.path.join(path, file)
-            mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
-            new_file = mp.AudioFileClip(mp4_path)
-            new_file.write_audiofile(mp3_path)
-            os.remove(mp4_path)
-else:
-    print("Successful!")
+    print("\nDownloading...")
+    yt.streams.get_highest_resolution().download(path)
+    print("Download completed.")
+
+    if option == 'a':
+        print("Converting...")
+        for file in os.listdir(path):
+            if re.search('mp4', file):
+                mp4_path = os.path.join(path, file)
+                mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
+                new_file = mp.AudioFileClip(mp4_path)
+                new_file.write_audiofile(mp3_path)
+                os.remove(mp4_path)
+    else:
+        print("\nSuccessful!")
+
+    sleep(1)
+    os.system('cls')
